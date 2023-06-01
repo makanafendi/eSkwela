377baddb22c370021ccb77b62d404134fdb28111
Jay-R Policarpio <88419583+makanafendi@users.noreply.github.com>
2023-06-01 12:36:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// ELEMENT TOGGLE FUNCTION
+
+const toggleElem = function (elem) { elem.classList.toggle("active"); }
+
+// NAVBAR TOGGLE
+
+const navbar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+for (let i = 0; i < navTogglers.length; i++) {
+  navTogglers[i].addEventListener("click", function () {
+    toggleElem(navbar);
+    toggleElem(overlay);
+  });
+}
+
+// HEADER STICKY & BACK TO TOP BUTTON
+
+const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    backTopBtn.classList.add("active");
+    header.classList.add("header-anim");
+  } else {
+    header.classList.remove("active");
+    backTopBtn.classList.remove("active");
+    header.classList.remove("header-anim");
+  }
+});
+
+// SEARCH BOX TOGGLE
+
+const searchTogglers = document.querySelectorAll("[data-search-toggler]");
+const searchBox = document.querySelector("[data-search-box]");
+
+for (let i = 0; i < searchTogglers.length; i++) {
+  searchTogglers[i].addEventListener("click", function () {
+    toggleElem(searchBox);
+  });
+}
+
+
+
+// WAITLIST TOGGLE BUTTON
+
+const whishlistBtns = document.querySelectorAll("[data-whish-btn]");
+
+for (let i = 0; i < whishlistBtns.length; i++) {
+  whishlistBtns[i].addEventListener("click", function () {
+    toggleElem(this);
+  });
+}
